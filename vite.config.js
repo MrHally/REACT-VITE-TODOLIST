3f23ec82419e1712f6bbdb011f5ce3d18cd812cf
MrHally <nhatanh.personal@gmail.com>
2024-03-05 01:51:46 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/REACT-VITE-TODOLIST/"
+  base: '/REACT-VITE-TODOLIST/'
 })
